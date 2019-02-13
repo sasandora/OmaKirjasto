28b67ccae0e5c231bcec177b7d014e3e38aa1611
sasandora <sasandora>
2019-02-13 13:13:39 +0200

    Eka kommit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+@include('head')
+
+<body>
+
+
+    <body>
+            @include('navbar')
+            @include('lorem')
+
+     </body>
+</body>
+
+</html>
