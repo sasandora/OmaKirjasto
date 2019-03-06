6b95abf04659491c2725b28bb77850a50b180dcc
sasandora <kalma5663@gmail.com>
2019-03-06 14:53:32 +0200

    Navbarin otsikot muutettu

--- resources/views/navigaatio/navbar.blade.php
+++ resources/views/navigaatio/navbar.blade.php
@@ -1,77 +1,68 @@
-    <!--Mobile navbar-->
-    <div class="d-md-none">
-        <nav class="navbar navbar-toggleable-md navbar-dark bg-dark fixed-top">
-            <div class="container ">
-                <div class="row">
-                    <div class="col">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
+<!--Mobile navbar-->
+<div class="d-md-none">
+    <nav class="navbar navbar-toggleable-md navbar-dark bg-dark fixed-top">
+        <div class="container ">
+            <div class="row">
+                <div class="col">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
 
-                    <div class="col d-flex ">
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item ">
-                                    <a class="nav-link" href="index.php">Home </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="History.htm">Historia</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="keikat.htm"><span
-                                            class="sr-only">(current)</span>Keikat</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Kauppa</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="media.htm">Media</a>
-                                </li>
-                            </ul>
-                        </div>
+                <div class="col d-flex ">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item ">
+                                <a class="nav-link" href="index.php">Etusivu </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="History.htm">Kirjat</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="keikat.htm">Tilaukset</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Kirjoittajat </a>
+                            </li>
+
                     </div>
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+</div>
 
-    <!--Large navbar-->
-    <div class="d-none d-md-block">
-        <nav class="navbar navbar-dark bg-dark fixed-top">
-            <div class="container">
-                <ul class="navbar-nav">
-                    <div class="row ">
-                        <div class="col d-flex justify-content-center">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home </a>
-                            </li>
-                        </div>
-                        <div class="col">
-                            <li class="nav-item">
-                                <a class="nav-link" href="History.htm">Historia</a>
-                            </li>
-                        </div>
-                        <div class="col">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="keikat.htm"> Keikat <span
-                                        class="sr-only">(current)</span>
-                                </a>
-                            </li>
-                        </div>
-                        <div class="col">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Kauppa</a>
-                            </li>
-                        </div>
-                        <div class="col">
-                            <li class="nav-item">
-                                <a class="nav-link" href="media.htm">Media</a>
-                            </li>
-                        </div>
+<!--Large navbar-->
+<div class="d-none d-md-block">
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <ul class="navbar-nav">
+                <div class="row ">
+                    <div class="col d-flex justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Etusivu </a>
+                        </li>
                     </div>
-                </ul>
-            </div>
-        </nav>
-    </div>
+                    <div class="col">
+                        <li class="nav-item">
+                            <a class="nav-link" href="History.htm">Kirjat</a>
+                        </li>
+                    </div>
+                    <div class="col">
+                        <li class="nav-item">
+                            <a class="nav-link " href="keikat.htm"> Tilaukset
+                            </a>
+                        </li>
+                    </div>
+                    <div class="col">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kirjoittajat </a>
+                        </li>
+                    </div>
+                   
+                </div>
+            </ul>
+        </div>
+    </nav>
+</div>
